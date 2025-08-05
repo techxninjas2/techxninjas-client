13624fe35f45bf8309b2f66fe0f8740a2ca30fbc
Vaishnavibansal21 <Vaishnavibansal2023@gmail.com>
2025-08-05 16:31:38 +0530

    Add the new name in index.tsx

--- index.tsx
+++ index.tsx
@@ -4,13 +4,13 @@ import App from './App';
 import { ThemeProvider } from './contexts/ThemeContext';
 import { AuthProvider } from './contexts/AuthContext';
 // Adding for tracking of referral code
-import ReactGA from 'react-ga4';
+import ReactGa from 'react-ga4';
 
 // ✅ Initialize GA4 with your ID to track the Referral Code as UTM medium and campaign
-ReactGA.initialize('G-7M7G6QM7Q2');
+ReactGa.initialize('G-7M7G6QM7Q2');
 
 // ✅ Track the initial pageview for REFERRAL
-ReactGA.send('pageview');
+ReactGa.send('pageview');
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
